8eb6b0777c27bbf42d862dd6b82a2685babcc736
mukesh <mukrsh199894@gmail.com>
2022-06-15 00:28:02 +0530

    email_password

--- mac/mac/info.py
+++ mac/mac/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mukeshgaur199931@gmail.com'
-EMAIL_HOST_PASSWORD = '8447070601'
+EMAIL_HOST_PASSWORD = 'oondgcaorwlkdqin'
 EMAIL_PORT = 587
